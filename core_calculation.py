3a459b2cc0337f976c92d8be9ab5b34030be3973
chiyodaluiz-blip <chiyoda.luiz@gmail.com>
2026-03-07 22:28:00 +0000

    Update core_calculation.py

--- core_calculation.py
+++ core_calculation.py
@@ -401,9 +401,8 @@ def run_notas(directory, brokerHouse):
                 file = os.path.join(directory, filename)
 
                 # Get the number of pages
-				with open(file, "rb") as f:
-				    pdf = PdfReader(f)
-				    n_pages = len(pdf.pages)
+		        pdf = PdfReader(file)
+		        n_pages = len(pdf.pages)
 
                 # flag to check if there are more than 1 page of trades in the day
                 last_date = '01/01/1900'
